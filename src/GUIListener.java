3d459f000cc2575eae84aca5c699ddad61af9e3a
dcturne1 <dcturne1@coastal.edu>
2013-11-20 21:38:16 -0500

    Layout Manager Set
    Set the Layout Manager with BoxLayout and fixed button size issue.

--- src/GUIListener.java
+++ src/GUIListener.java
@@ -1,8 +1,10 @@
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,32 +15,38 @@ import javax.swing.JTextField;
 
 public class GUIListener extends JPanel
 {
-	static JTextArea textArea;
+	static JTextArea textArea = new JTextArea(3, 30);
 	
 	public GUIListener() {
         
         JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         
         JScrollPane scrollPane = new JScrollPane(textArea);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setPreferredSize(new Dimension (20, 100));
+        scrollPane.setPreferredSize(new Dimension (250, 250));
+        scrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
         
-        textArea = new JTextArea(10, 50);
         textArea.setEditable(false);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         
         JLabel label = new JLabel("Would you like to play a game of Rock, Paper, Scissors?");
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         JButton buttonY = new JButton("Yes");
-        buttonY.setAlignmentX(Component.LEFT_ALIGNMENT);
         buttonY.setPreferredSize(new Dimension(225, 80));
+        buttonY.setMinimumSize(new Dimension(225, 80));
+        buttonY.setMaximumSize(new Dimension(225, 80));
+        buttonY.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
         JButton buttonN = new JButton("No");
-        buttonN.setAlignmentX(Component.RIGHT_ALIGNMENT);
         buttonN.setPreferredSize(new Dimension(225, 80));
+        buttonN.setMinimumSize(new Dimension(225, 80));
+        buttonN.setMaximumSize(new Dimension(225, 80));
+        buttonN.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         panel.add(label);
-        panel.add(textArea);
         panel.add(scrollPane);
         panel.add(buttonY);
         panel.add(buttonN);
@@ -47,7 +55,7 @@ public class GUIListener extends JPanel
     }
 	
 	public void textUpdate(String temp) {
-		//textArea.setCaretPosition(0);
+		textArea.setCaretPosition(0);
 		textArea.append(temp + "\n");
 		textArea.setCaretPosition(textArea.getDocument().getLength());
 	}
